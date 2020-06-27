352d53b965fb6e351217923067b8a8e3481d1e61
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-06-27 14:58:01 -0400

    Carpeta entidades y sus clases

---
+++ Prestamo con Detalla/Entidades/MorasDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Prestamo_con_Detalla.Entidades
+{
+    class MorasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PrestamoId { get; set; }
+        public string Requerimiento { get; set; }
+        public float Valor { get; set; }
+        public MorasDetalle(int Prestamoid, string requerimiento, float valor)
+        {
+            Id = 0;
+            PrestamoId = Prestamoid;
+            Requerimiento = requerimiento;
+            Valor = valor;
+        }
+    }
+}
---
+++ Prestamo con Detalla/Entidades/Prestamos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Prestamo_con_Detalla.Entidades
+{
+    class Prestamos
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int total { get; set; }
+
+
+        [ForeignKey("IdArticulo")]
+        public List<MorasDetalle> Detalle { get; set; } = new List<MorasDetalle>();
+    }
+}
+    }
+}
